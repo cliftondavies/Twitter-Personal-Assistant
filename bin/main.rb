1b04fc029a2c1f30c513e8c6c6d79deacdb46a82
cliftondavies <clifton.davies@outlook.com>
2020-05-06 00:01:26 +0100

    Run credentials test

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+require 'dotenv/load'
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['API_KEY']
+  config.consumer_secret     = ENV['API_KEY_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+client.update("Test One!")
