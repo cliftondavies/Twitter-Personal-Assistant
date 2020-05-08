54a0fe9c61618c098b73c10790a77f1a9246e364
cliftondavies <clifton.davies@outlook.com>
2020-05-08 20:22:49 +0100

    Add store_tweet method to user module

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+require 'dotenv/load'
+require 'twitter'
+require 'yaml'
+
+module User
+  def self.store_tweet(tweets = [])
+    tweets += client.user_timeline('qualitycodebot', count: 1)
+    File.write('tweets.yml', YAML.dump(tweets))
+  end
+end
