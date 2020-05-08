b4f3569b9533a4b69c479da5c95690b1213d641d
cliftondavies <clifton.davies@outlook.com>
2020-05-08 22:31:46 +0100

    Edit flow for like_retweet in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -27,13 +27,15 @@ if response == 'y'
   puts 'Please type in your tweet'
   tweet = gets.chomp
   client.update(tweet)
-  puts "You tweeted #{tweet.length} characters."
+  puts 'Tweet successful!'
+  puts "Tweet content: #{tweet}"
+  puts "No. of characters: #{tweet.length}"
 end
 
 # Add tweet to record if tweet was made --refactor --def store_tweet(tweets = [])
 # if first tweet -- when client.user.tweets_count.zero? call store_tweet
 puts 'Storing tweet...'
-client.user.tweets_count.zero ? store_tweet : store_tweet(YAML.load_file('tweets.yml'))
+client.user.tweets_count.zero? ? store_tweet : store_tweet(YAML.load_file('tweets.yml'))
 
 # tweets = client.user_timeline('qualitycodebot', count: 1)
 # File.write('tweets.yml', YAML.dump(tweets))
@@ -62,11 +64,17 @@ puts "You have #{client.user.followers_count} follower(s)."
 # Like a retweet --def like_retweets(tweets = [], fav_tweets = [])
 # tweets = YAML.load_file('tweets.yml')
 # fav_tweets = [] # put in parameter
-tweets.each do |twit|
-  unliked_tweets = client.retweets(twit) - fav_tweets
-  fav_tweets += client.fav(unliked_tweets) unless unliked_tweets.size.zero?
+if client.user.tweets_count.zero? || retweets_received.zero?
+  puts 'You have not received any retweets'
+elsif YAML.load_file('fav_tweets.yml').is_a?(Array)
+  like_retweets(YAML.load_file('tweets.yml'), YAML.load_file('fav_tweets.yml'))
+else
+  like_retweets
 end
-File.write('fav_tweets.yml', YAML.dump(fav_tweets)) # should i put this in new method called store_fav_tweets(fav_tweets)?
+# tweets.each do |twit|
+#   unliked_tweets = client.retweets(twit) - fav_tweets
+#   fav_tweets += client.fav(unliked_tweets) unless unliked_tweets.empty?
+# File.write('fav_tweets.yml', YAML.dump(fav_tweets)) # should i put this in new method called store_fav_tweets(fav_tweets)?
 
 # count retweet likes given --def retweets_liked(fav_tweets)
 fav_tweets = YAML.load_file('fav_tweets.yml')
