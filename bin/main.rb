cebdf287313d94ece9ad95019c58aba06a63ce45
cliftondavies <clifton.davies@outlook.com>
2020-05-11 21:46:42 +0100

    Update flow for main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -8,13 +8,8 @@ require 'yaml'
 client = Twitter::REST::Client.new(Client.config)
 
 # Tweet
-puts 'Would you like to tweet? Enter y/n'
-valid = %w[y n]
+puts "Would you like to tweet? Enter 'y' for 'yes', or press any other key to skip"
 response = gets.chomp.downcase
-until valid.include?(response)
-  puts 'Typo: Please enter y/n'
-  response = gets.chomp.downcase
-end
 if response == 'y'
   puts 'Please type in your tweet'
   tweet = gets.chomp
@@ -30,35 +25,47 @@ else
 end
 
 # Like retweets
-puts 'Would you like to favorite some retweets of your posts that you have not liked? Enter y/n'
-if client.user.tweets_count.zero? || retweets_received.zero?
-  puts 'You have not received any retweets'
-elsif YAML.load_file('fav_tweets.yml').is_a?(Array)
-  like_retweets(YAML.load_file('tweets.yml'), YAML.load_file('fav_tweets.yml'))
+puts 'Would you like to favorite some retweets of your posts that you have not liked?'
+puts "Enter 'y' for 'yes', or press any other key to skip"
+reply = gets.chomp.downcase
+if reply == 'y'
+  if client.user.tweets_count.zero? || retweets_received.zero?
+    puts 'You have not received any retweets'
+  elsif YAML.load_file('fav_tweets.yml').is_a?(Array)
+    like_retweets(YAML.load_file('tweets.yml'), YAML.load_file('fav_tweets.yml'))
+  else
+    like_retweets
+  end
 else
-  like_retweets
+  puts 'Nooo?! Your retweeters will be disaapointed.'
 end
 
-puts 'Would you like to see your account summary? y/n'
-# Total followers
-puts "You have #{client.user.followers_count} follower(s)."
+puts 'Would you like to see your account summary?'
+puts "Enter 'y' for 'yes', or press any other key to skip"
+choice = gets.chomp.downcase
+if choice == 'y'
+  # Total followers
+  puts "You have #{client.user.followers_count} follower(s)."
 
-# Total tweets
-puts "You have tweeted #{client.user.tweets_count} time(s)."
+  # Total tweets
+  puts "You have tweeted #{client.user.tweets_count} time(s)."
 
-if YAML.load_file('tweets.yml').is_a?(Array)
-  # Total retweets received
-  puts "#{retweets_received(YAML.load_file('tweets.yml'))} of your tweets have been retweeted."
+  if YAML.load_file('tweets.yml').is_a?(Array)
+    # Total retweets received
+    puts "#{retweets_received(YAML.load_file('tweets.yml'))} of your tweets have been retweeted."
 
-  # Total likes received
-  puts "#{likes_received(YAML.load_file('tweets.yml'))} of your tweets have been liked."
+    # Total likes received
+    puts "#{likes_received(YAML.load_file('tweets.yml'))} of your tweets have been liked."
+  else
+    puts 'You have not received any tweet likes or retweets.'
+  end
+
+  # Total retweets liked
+  if YAML.load_file('fav_tweets.yml').is_a?(Array)
+    puts "You have liked #{YAML.load_file('fav_tweets.yml').size} retweets of your tweets"
+  else
+    puts 'You either have not liked, or have not received, any retweets of your tweets'
+  end
 else
-  puts 'You have not received any tweet likes or retweets.'
-end
-
-# Total retweets liked
-if YAML.load_file('fav_tweets.yml').is_a?(Array)
-  puts "You have liked #{YAML.load_file('fav_tweets.yml').size} retweets of your tweets"
-else
-  puts 'You either have not liked, or have not received, any retweets of your tweets'
+  puts 'The stats will be here when you need them.'
 end
