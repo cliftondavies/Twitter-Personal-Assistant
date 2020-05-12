f17dd67ead16122c6735c587571836c52b664e48
cliftondavies <clifton.davies@outlook.com>
2020-05-12 03:44:08 +0100

    Add specs for client module

---
+++ spec/client_spec.rb
@@ -0,0 +1,9 @@
+require './lib/client.rb'
+
+describe Client do
+  describe '.config' do
+    it 'returns the client config info in the form of a hash' do
+      expect(Client.config.keys).to eql(%i[consumer_key consumer_secret access_token access_token_secret])
+    end
+  end
+end
