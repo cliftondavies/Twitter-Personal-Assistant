5fccb1cd9330e5bf0e8911ef00643b09bd8fb878
cliftondavies <clifton.davies@outlook.com>
2020-05-13 21:57:33 +0100

    Update main.rb with User.mentions

--- bin/main.rb
+++ bin/main.rb
@@ -51,29 +51,29 @@ puts "
 -------------
 LIKE RETWEETS
 -------------
-Q: Would you like to favorite some retweets of your posts that you have not liked?
+Q: Would you like to favorite some tweets that mentioned you?
 >> Enter 'y' for 'yes', or press any other key to skip
 "
 reply = gets.chomp.downcase
 puts ' '
 if reply == 'y'
-  if Client::C.user.tweets_count.zero? || User.retweets_received(YAML.load_file('tweets.yml')).zero?
-    puts '| You have not received any retweets |'
-  elsif YAML.load_file('fav_tweets.yml').is_a?(Array)
-    puts '| Checking for unliked retweet... |
+  # if Client::C.user.tweets_count.zero? || User.retweets_received(YAML.load_file('tweets.yml')).zero?
+    # puts '| You have not received any retweets |'
+  if YAML.load_file('mentions.yml').is_a?(Array)
+    puts '| Checking for mentions... |
     '
     sleep 2
-    User.like_retweets(YAML.load_file('tweets.yml'), YAML.load_file('fav_tweets.yml'))
+    User.like_mentions(YAML.load_file('mentions.yml'))
     puts 'Done.'
   else
     puts '| Checking for unliked retweet... |
     '
     sleep 2
-    User.like_retweets(YAML.load_file('tweets.yml'))
+    User.like_mentions
     puts 'Done.'
   end
 else
-  puts '| Nooo?! Your retweeters will be disappointed. |'
+  puts '| Nooo?! Those who mentioned you will be disappointed. |'
 end
 
 puts "
@@ -114,8 +114,8 @@ if choice == 'y'
 
   # Total retweets liked
   if YAML.load_file('fav_tweets.yml').is_a?(Array)
-    puts "| Total Retweets Liked: |
-    You have liked #{YAML.load_file('fav_tweets.yml').size} retweet(s) of your tweets.
+    puts "| Total Mentions Liked: |
+    You have liked #{YAML.load_file('mentions.yml').size} retweet(s) of your tweets.
     "
   else
     puts '| You either have not liked, or have not received, any retweets of your tweets |'
