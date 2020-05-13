6e2d5d265fb8e80f1a6de0b061048ae4b9b2047e
cliftondavies <clifton.davies@outlook.com>
2020-05-13 13:40:47 +0100

    Refactor user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,8 @@ require 'twitter'
 require './lib/user.rb'
 
 describe User do
+  let(:tweets) { Client::C.user_timeline(Client::C.user.id) }
+
   describe '.store_tweet' do
     context 'when user makes first tweet' do
       it 'returns an array containing first tweet' do
@@ -11,7 +13,6 @@ describe User do
 
     context 'when user makes subsequent tweets' do
       it 'returns an array with subsequent tweets' do
-        tweets = Client::C.user_timeline(Client::C.user.id)
         expect(User.store_tweet(tweets)).to be_an(Array)
       end
     end
@@ -20,14 +21,12 @@ describe User do
   describe '.like_retweets' do
     context 'when user has no favorite tweets' do
       it 'returns an array with result of running like_retweets' do
-        tweets = Client::C.user_timeline(Client::C.user.id)
         expect(User.like_retweets(tweets)).to be_an(Array)
       end
     end
 
     context 'when user has favorite tweets' do
       it 'returns an array with result of running like_retweets' do
-        tweets = Client::C.user_timeline(Client::C.user.id)
         fav_tweets = Client::C.favorites(Client::C.user.id)
         expect(User.like_retweets(tweets, fav_tweets)).to be_an(Array)
       end
@@ -36,14 +35,12 @@ describe User do
 
   describe '.retweets_received' do
     it "returns the number of times the user's tweets have been retweeted, as an integer" do
-      tweets = Client::C.user_timeline(Client::C.user.id)
       expect(User.retweets_received(tweets)).to be_an(Integer)
     end
   end
 
   describe '.likes_received' do
     it "returns the number of times the user's tweets have been liked, as an integer" do
-      tweets = Client::C.user_timeline(Client::C.user.id)
       expect(User.likes_received(tweets)).to be_an(Integer)
     end
   end
