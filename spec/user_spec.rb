364cf8a50ad8b0d268ff8658915d362b4e896c19
cliftondavies <clifton.davies@outlook.com>
2020-05-15 16:54:53 +0100

    Change user.spec.rb to avoid overwriting of tweets and mentions file

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,21 +3,24 @@ require 'yaml'
 require './lib/user.rb'
 
 describe User do
-  let(:tweets) { YAML.load_file('tweets_testdata.yml') }
+  let(:test_tweets) { YAML.load_file('tweets_testdata.yml') }
 
   describe '.store_tweet' do
     context 'when user makes first tweet' do
       it 'returns an array containing first tweet' do
+        tweets_record = YAML.load_file('tweets.yml')
         expect(User.store_tweet).to be_an(Array)
-        tweets = YAML.load_file('tweets.yml')
-        tweets.pop
-        tweets.empty? ? File.write('tweets.yml', YAML.dump(nil)) : File.write('tweets.yml', YAML.dump(tweets))
+        if tweets_record
+          File.write('tweets.yml', YAML.dump(tweets_record))
+        else
+          File.write('tweets.yml', YAML.dump(nil))
+        end
       end
     end
 
     context 'when user makes subsequent tweets' do
       it 'returns an array with subsequent tweets' do
-        expect(User.store_tweet(tweets)).to be_an(Array) | eql(nil)
+        expect(User.store_tweet(test_tweets)).to be_an(Array) | eql(nil)
       end
     end
   end
@@ -25,30 +28,33 @@ describe User do
   describe '.like_mentions' do
     context 'when user has no mentions stored' do
       it 'returns an array with result of running like_mentions' do
+        mentions_record = YAML.load_file('mentions.yml')
         expect(User.like_mentions).to be_an(Array)
-        mentions = YAML.load_file('mentions.yml')
-        mentions.pop
-        mentions.empty? ? File.write('mentions.yml', YAML.dump(nil)) : File.write('mentions.yml', YAML.dump(mentions))
+        if mentions_record
+          File.write('mentions.yml', YAML.dump(mentions_record))
+        else
+          File.write('mentions.yml', YAML.dump(nil))
+        end
       end
     end
 
     context 'when user has mentions stored' do
       it 'returns an array with result of running like_mentions' do
-        mentions = YAML.load_file('mentions_testdata.yml')
-        expect(User.like_mentions(mentions)).to be_an(Array) | eql(nil)
+        test_mentions = YAML.load_file('mentions_testdata.yml')
+        expect(User.like_mentions(test_mentions)).to be_an(Array) | eql(nil)
       end
     end
   end
 
   describe '.retweets_received' do
     it "returns the number of times the user's tweets have been retweeted, as an integer" do
-      expect(User.retweets_received(tweets)).to be_an(Integer)
+      expect(User.retweets_received(test_tweets)).to be_an(Integer)
     end
   end
 
   describe '.likes_received' do
     it "returns the number of times the user's tweets have been liked, as an integer" do
-      expect(User.likes_received(tweets)).to be_an(Integer)
+      expect(User.likes_received(test_tweets)).to be_an(Integer)
     end
   end
 end
