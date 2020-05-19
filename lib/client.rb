e1691d9760be5b4d98c4a3c5aa9d61d6ac7998f4
cliftondavies <clifton.davies@outlook.com>
2020-05-19 15:59:29 +0100

    Change Client module to class

--- lib/client.rb
+++ lib/client.rb
@@ -1,7 +1,11 @@
 require 'dotenv/load'
 
-module Client
-  def self.config
+class Client
+  C = Twitter::REST::Client.new(config)
+
+  private
+
+  def config
     {
       consumer_key: ENV['API_KEY'],
       consumer_secret: ENV['API_KEY_SECRET'],
@@ -9,6 +13,17 @@ module Client
       access_token_secret: ENV['ACCESS_TOKEN_SECRET']
     }
   end
-
-  C = Twitter::REST::Client.new(config)
 end
+
+# module Client
+#   def self.config
+#     {
+#       consumer_key: ENV['API_KEY'],
+#       consumer_secret: ENV['API_KEY_SECRET'],
+#       access_token: ENV['ACCESS_TOKEN'],
+#       access_token_secret: ENV['ACCESS_TOKEN_SECRET']
+#     }
+#   end
+
+#   C = Twitter::REST::Client.new(config)
+# end
