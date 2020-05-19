23a7a1b59427b8f86e8f6cc97c682dc49b0ea04a
cliftondavies <clifton.davies@outlook.com>
2020-05-19 18:18:27 +0100

    Remove commented out code

--- lib/client.rb
+++ lib/client.rb
@@ -12,16 +12,3 @@ class Client
 
   C = Twitter::REST::Client.new(config)
 end
-
-# module Client
-#   def self.config
-#     {
-#       consumer_key: ENV['API_KEY'],
-#       consumer_secret: ENV['API_KEY_SECRET'],
-#       access_token: ENV['ACCESS_TOKEN'],
-#       access_token_secret: ENV['ACCESS_TOKEN_SECRET']
-#     }
-#   end
-
-#   C = Twitter::REST::Client.new(config)
-# end
